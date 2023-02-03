1e9b1bd806fd386b4579a7d59c4589c708030c8d
aniskehila <aniskehila22@gmail.com>
2023-02-03 02:13:52 +0100

    add fields & logic for players names

--- src/main.js
+++ src/main.js
@@ -10,124 +10,133 @@ const gameBoard = (() => {
     const replayBtn = document.querySelector('#replay');
     const homeBtn = document.querySelector('#quit');
     const players = document.querySelectorAll('.player');
-    let boardState = new Array(cells.length);
-    boardState.fill();
     const clickSound = new Audio("src/sounds/click.wav");
+    let boardState = new Array(cells.length).fill();
     let note;
-    startGameBtn.addEventListener('click', () => {
-        entrePage.classList.add('d-none');
-        gameBoard.classList.remove('d-none');
-        players.forEach(player => player.classList.remove('d-none'));
-    });
+    let playerOneName = 'Player One';
+    let playerTwoName = 'Player Two';
     class player {
         constructor(name, mark) {
             this.name = name;
             this.mark = mark;
         }
     }
-    let playerOne = new player('Player One', 'x');
-    let playerTwo = new player('Player Two', 'o');
-    let playerTurn = playerOne;
-    function switchTurns() {
-        if(playerTurn === playerOne) {
-            playerTurn = playerTwo;  
-        } else {
-            playerTurn = playerOne;
+    startGameBtn.addEventListener('click', () => {
+        entrePage.classList.add('d-none');
+        gameBoard.classList.remove('d-none');
+        players.forEach(player => player.classList.remove('d-none'));
+        if(document.querySelector('#player-one-name').value !== '') {
+            playerOneName = document.querySelector('#player-one-name').value;
         }
-    }
-    const winCombos = [
-        {combo: [0,1,2] , classEvent: 'win-line-hor-1'},
-        {combo: [3,4,5] , classEvent: 'win-line-hor-2'},
-        {combo: [6,7,8] , classEvent: 'win-line-hor-3'},
-        {combo: [0,3,6] , classEvent: 'win-line-ver-1'},
-        {combo: [1,4,7] , classEvent: 'win-line-ver-2'},
-        {combo: [2,5,8] , classEvent: 'win-line-ver-3'},
-        {combo: [0,4,8] , classEvent: 'win-line-deg-1'},
-        {combo: [2,4,6] , classEvent: 'win-line-deg-2'}
-    ];
-    function hoverEffect() {
-        cells.forEach(cell => {
-            cell.classList.remove('hover-x');
-            cell.classList.remove('hover-o');
+        if(document.querySelector('#player-two-name').value !== '') {
+            playerTwoName = document.querySelector('#player-two-name').value;
+        }
+        let playerOne = new player(playerOneName, 'x');
+        let playerTwo = new player(playerTwoName, 'o');
+        players[0].innerText = playerOne.name;
+        players[1].innerText = playerTwo.name;
+        let playerTurn = playerOne;
+        function switchTurns() {
+            if(playerTurn === playerOne) {
+                playerTurn = playerTwo;  
+            } else {
+                playerTurn = playerOne;
+            }
+        }
+        const winCombos = [
+            {combo: [0,1,2] , classEvent: 'win-line-hor-1'},
+            {combo: [3,4,5] , classEvent: 'win-line-hor-2'},
+            {combo: [6,7,8] , classEvent: 'win-line-hor-3'},
+            {combo: [0,3,6] , classEvent: 'win-line-ver-1'},
+            {combo: [1,4,7] , classEvent: 'win-line-ver-2'},
+            {combo: [2,5,8] , classEvent: 'win-line-ver-3'},
+            {combo: [0,4,8] , classEvent: 'win-line-deg-1'},
+            {combo: [2,4,6] , classEvent: 'win-line-deg-2'}
+        ];
+        function hoverEffect() {
+            cells.forEach(cell => {
+                cell.classList.remove('hover-x');
+                cell.classList.remove('hover-o');
+                if(cell.innerText == '') {
+                    cell.classList.add(`hover-${playerTurn.mark}`);
+                } else {
+                    return;
+                }
+            });
+        }
+        function writeIntoBoard(cell) {
             if(cell.innerText == '') {
-                cell.classList.add(`hover-${playerTurn.mark}`);
+                cell.innerText = playerTurn.mark;
+                clickSound.play();
+                clickSound.currentTime = 0;
             } else {
                 return;
             }
-        });
-    }
-    function writeIntoBoard(cell) {
-        if(cell.innerText == '') {
-            cell.innerText = playerTurn.mark;
-            clickSound.play();
-            clickSound.currentTime = 0;
-        } else {
-            return;
         }
-    }
-    function checkForWinner() {
-        for(const comboObj of winCombos) {
-            let {combo , classEvent} = comboObj;
-            let value1 = boardState[combo[0]];
-            let value2 = boardState[combo[1]];
-            let value3 = boardState[combo[2]];
-            if(value1 !== undefined && value1 === value2 && value2 === value3) {
-                winLine.classList.add(classEvent);
+        function checkForWinner() {
+            for(const comboObj of winCombos) {
+                let {combo , classEvent} = comboObj;
+                let value1 = boardState[combo[0]];
+                let value2 = boardState[combo[1]];
+                let value3 = boardState[combo[2]];
+                if(value1 !== undefined && value1 === value2 && value2 === value3) {
+                    winLine.classList.add(classEvent);
+                    return true;
+                }
+            }
+        }
+        function gameEnds() {
+            if(checkForWinner()) {
+                playerTurn === playerOne ? note = `GameOver ${playerTwo.name} Won!` : note = `GameOver ${playerOne.name} Won!`; 
                 return true;
+            } else if(boardState.every(cell => cell === 'x' || cell === 'o')) {
+                note = `GameOver It\'s A Draw!`; 
+                return true;   
+            } else {
+                return false;
             }
         }
-    }
-    function gameEnds() {
-        if(checkForWinner()) {
-            playerTurn === playerOne ? note = `GameOver ${playerTwo.name} Won!` : note = `GameOver ${playerOne.name} Won!`; 
-            return true;
-        } else if(boardState.every(cell => cell === 'x' || cell === 'o')) {
-            note = `GameOver It\'s A Draw!`; 
-            return true;   
-        } else {
-            return false;
-        }
-    }
-    function boardHandler(event) {
-        if(!boardState.includes(event.target.dataset.position)) {
-            if(!gameEnds()) {
-                writeIntoBoard(event.target);
-                boardState[event.target.dataset.position] = playerTurn.mark;
-                switchTurns();
-                players.forEach(player => {
-                    player.classList.remove('active');
-                    if(player.dataset.player === playerTurn.name) {
-                        player.classList.add('active');
+        function boardHandler(event) {
+            if(!boardState.includes(event.target.dataset.position)) {
+                if(!gameEnds()) {
+                    writeIntoBoard(event.target);
+                    boardState[event.target.dataset.position] = playerTurn.mark;
+                    switchTurns();
+                    for(const player of players) {
+                        player.classList.remove('active');
+                        if(player.innerText === playerTurn.name) {
+                            player.classList.add('active');
+                        }
                     }
-                })
-                hoverEffect();
+                    hoverEffect();
+                }
+                if(gameEnds()) {
+                    resContainer.classList.remove('d-none')
+                    result.innerText = note;
+                }
+            } else {
+                return;
             }
-            if(gameEnds()) {
-                resContainer.classList.remove('d-none')
-                result.innerText = note;
-            }
-        } else {
-            return;
         }
-    }
-    function resetGame() {
-        boardState.fill(undefined);
-        cells.forEach(cell => cell.innerText = '');
-        resContainer.classList.add('d-none');
-        winLine.className = '';
-        hoverEffect();
-    }
-    function backHome() {
-        resetGame();
-        gameBoard.classList.add('d-none');
-        players.forEach(player => player.classList.add('d-none'));
-        entrePage.classList.remove('d-none');
-    }
-    cells.forEach(cell => {
-        cell.addEventListener('click', boardHandler);
-        hoverEffect();
+        function resetGame() {
+            boardState.fill(undefined);
+            cells.forEach(cell => cell.innerText = '');
+            resContainer.classList.add('d-none');
+            winLine.className = '';
+            hoverEffect();
+        }
+        function backHome() {
+            resetGame();
+            gameBoard.classList.add('d-none');
+            players.forEach(player => player.classList.add('d-none'));
+            entrePage.classList.remove('d-none');
+        }
+        cells.forEach(cell => {
+            cell.addEventListener('click', boardHandler);
+            hoverEffect();
+        });
+        replayBtn.addEventListener('click' , resetGame);
+        homeBtn.addEventListener('click',backHome);
     });
-    replayBtn.addEventListener('click' , resetGame);
-    homeBtn.addEventListener('click',backHome);
 });
 gameBoard();
